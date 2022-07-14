00b5f6cf4cabcf264d950dbba0cb13327a0c3faa
suhongkim98 <kshz9815@naver.com>
2022-07-14 14:59:28 +0900

    chore: init commit

---
+++ src/test/java/com/devnus/belloga/labeling/LabelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnus.belloga.labeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
