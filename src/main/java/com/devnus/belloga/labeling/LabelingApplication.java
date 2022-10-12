421aff884adfed1ac2b3eca22af08446b73df612
suhongkim98 <kshz9815@naver.com>
2022-10-12 16:24:07 +0900

    fix: base entity created date null

--- src/main/java/com/devnus/belloga/labeling/LabelingApplication.java
+++ src/main/java/com/devnus/belloga/labeling/LabelingApplication.java
@@ -2,8 +2,10 @@ package com.devnus.belloga.labeling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LabelingApplication {
 
 	public static void main(String[] args) {
